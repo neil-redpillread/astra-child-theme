e1bdbf331f62cd5ca7a0a89babb04000679a52be
neil-redpillread <redpillread@gmail.com>
2025-07-24 20:10:34 +0100

    php file of my portfolio page

---
+++ portfolio-page.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Portfolio Page
+*/
+get_header();
+?>
+<section class="portfolio-hero-section">
+   <div class="container">
+    <div class="portfolio-hero-content">
+    <h1>
+    Your Personal SEO Specialist
+    </h1>
+     <div class="video-wrapper">
+        <iframe src="https://www.youtube.com/embed/O7il3R-scF4" title="YouTube video" allowfullscreen></iframe>
+      </div>
+      <div class="text-content">
+          <p>Hi, I'm Neil Jose — a freelance SEO service provider and a graduate with a Master's in Digital Marketing from Trinity College Dublin. I've been working in the industry since 2019, building hands-on expertise across all areas of SEO. Hiring me as your SEO specialist means giving your small business website the best chance to rank on the first page of Google. Click the video to learn more about me.</p>
+          <a href="services/" class="cta-button">Explore Services</a>
+         </div>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
