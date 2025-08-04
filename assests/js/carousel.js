d8d6b6e94de13ce61f74cbe57a2f096552cfd08d
neil-redpillread <redpillread@gmail.com>
2025-08-04 13:43:47 +0100

    Adding a carousel to the portfolio page (stage 2)

---
+++ assests/js/carousel.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  new Swiper('.portfolio-carousel', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    breakpoints: {
+      768: { slidesPerView: 1 },
+      1024:{ slidesPerView: 2 }
+    }
+  });
+});
