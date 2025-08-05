2b5ba235260484e85627f1f19a4f3c2a323c3166
neil-redpillread <redpillread@gmail.com>
2025-08-05 16:29:45 +0100

    Updated carousel edits

--- assests/js/carousel.js
+++ assests/js/carousel.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  new Swiper('.portfolio-carousel', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 'auto',
-    spaceBetween: 24,
-    breakpoints: {
-      768: { slidesPerView: 1 },
-      1024:{ slidesPerView: 2 }
-    }
+    new Swiper('.portfolio-carousel', {
+      loop: true,
+      loopedSlides: 4,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      slidesPerView: 2,
+      spaceBetween: 40,
+      breakpoints: {
+        768:  { slidesPerView: 1 },
+        1024: { slidesPerView: 2 }
+      }
+    });
   });
-});
